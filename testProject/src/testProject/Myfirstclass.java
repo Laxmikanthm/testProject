d13dddcc61ac8144b08e541c6d55a6551dcf3397
lucky <laxmikanthm0108@gmail.com>
2018-12-06 12:24:11 +0530

    Did changes in Myfirst project

--- testProject/src/testProject/Myfirstclass.java
+++ testProject/src/testProject/Myfirstclass.java
@@ -1,5 +1,13 @@
 package testProject;
 
 public class Myfirstclass {
+	
+	
+	static int x=10;
+	static int y=20;
+	static int z=x+y;
+	public static void main(String[] args) {
+	
+	System.out.println("Sum of two number" +z);
 
-}
+}}
